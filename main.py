ebe0260b89aa921f077845f3d07b9a534f288762
Matei Andrei <mateiandreialexandru77@yahoo.com>
2025-02-23 13:50:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,13 +62,6 @@ check_image= PhotoImage(file="images/right.png")
 known_button= Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
-
 next_card()
-
-
-
-
-
-
 
 window.mainloop()
